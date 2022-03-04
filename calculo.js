f665febe5122bb7bf44856726df09d478ba24fc9
Kauan Lima <kauansaninanalima@gmail.com>
2022-03-03 21:43:21 -0300

    subir lógica

---
+++ calculo.js
@@ -0,0 +1,11 @@
+var valorum = 10;
+var valordois = 2;
+var valortres = 10;
+var total;
+
+//total = valorum + valordois;
+//total = valorum - valordois;
+//total = valorum * valordois;
+//total = valorum ** valordois;
+total = (valorum + valordois) / valortres
+console.log (total);
---
+++ nome.js
@@ -0,0 +1,9 @@
+//Exibir meu nome
+//console.log('Kauan');
+var nome = 'kauan ';
+var sobrenome = 'Lima ';
+//var nomecompleto;
+//var idade = 24;
+
+//nomecompleto = nome + sobrenome;
+console.log( "Meu nome é: "+" "+ nome + " "+sobrenome );
