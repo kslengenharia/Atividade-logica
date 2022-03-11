9e2ff8adb32dcbcae38b7aa232dd7d2240dc34a7
luizflaper <luizflaper@gmail.com>
2022-03-10 21:07:02 -0300

    atividade de logica

---
+++ atividade2.js
@@ -0,0 +1,12 @@
+const Listadepresenca = 14;
+
+for (let index = 0; index < Listadepresenca; index++) {
+    if (index==0){
+        console.log ('Zero'+index)
+    }else if (index % 2 ==0){
+        console.log ('o numero é par'+index)
+    }
+    else {
+    console.log('o numero é impar'+index)
+    }
+}
